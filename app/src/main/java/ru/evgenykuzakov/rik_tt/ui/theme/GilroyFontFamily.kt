5c3b65275e83b8ca299299f6ccbd2397d5776a0f
kuzak <kuzakovevgen@gmail.com>
2025-06-04 03:30:26 +0700

    Setting up the theme

---
+++ app/src/main/java/ru/evgenykuzakov/rik_tt/ui/theme/GilroyFontFamily.kt
@@ -0,0 +1,11 @@
+package ru.evgenykuzakov.rik_tt.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import ru.evgenykuzakov.rik_tt.R
+
+val GilroyFontFamily = FontFamily(
+    Font(R.font.gilroy_light, FontWeight.Light),
+    Font(R.font.gilroy_extra_bold, FontWeight.ExtraBold),
+)
--- app/src/main/java/ru/evgenykuzakov/rik_tt/ui/theme/Type.kt
+++ app/src/main/java/ru/evgenykuzakov/rik_tt/ui/theme/Type.kt
@@ -2,33 +2,44 @@ package ru.evgenykuzakov.rik_tt.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+    displayLarge =  TextStyle(
+        fontFamily = GilroyFontFamily,
+        fontWeight = FontWeight.W700,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = (-0.14).sp
+    ),
+    displayMedium =  TextStyle(
+        fontFamily = GilroyFontFamily,
+        fontWeight = FontWeight.W700,
+        fontSize = 20.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (-0.11).sp
+    ),
+    bodyMedium =  TextStyle(
+        fontFamily = GilroyFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 15.sp,
+        lineHeight = (15 * 1.16).sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    bodySmall =  TextStyle(
+        fontFamily = GilroyFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 13.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        letterSpacing = (-0.04).sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = GilroyFontFamily,
+        fontWeight = FontWeight.W500,
+        fontSize = 10.sp,
+        lineHeight = 11.sp,
+        letterSpacing = ( -10 * 0.0011 ).sp
+    ),
 )
