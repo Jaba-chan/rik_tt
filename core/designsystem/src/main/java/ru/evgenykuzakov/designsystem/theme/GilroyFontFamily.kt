01e8451d0e5ae7c6b5a20b4e18e0ec79de1d0a93
kuzak <kuzakovevgen@gmail.com>
2025-06-06 03:56:39 +0700

    Create legend for chart and added fonts

--- core/designsystem/src/main/java/ru/evgenykuzakov/designsystem/theme/GilroyFontFamily.kt
+++ core/designsystem/src/main/java/ru/evgenykuzakov/designsystem/theme/GilroyFontFamily.kt
@@ -2,10 +2,20 @@ package ru.evgenykuzakov.designsystem.theme
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import ru.evgenykuzakov.designsystem.R
 
 val GilroyFontFamily = FontFamily(
+    Font(R.font.gilroy_thin, FontWeight.Thin),
+    Font(R.font.gilroy_ultra_light, FontWeight.ExtraLight),
     Font(R.font.gilroy_light, FontWeight.Light),
+    Font(R.font.gilroy_regular, FontWeight.Normal),
+    Font(R.font.gilroy_medium, FontWeight.Medium),
+    Font(R.font.gilroy_semibold, FontWeight.SemiBold),
+    Font(R.font.gilroy_bold, FontWeight.Bold),
     Font(R.font.gilroy_extra_bold, FontWeight.ExtraBold),
-)
+    Font(R.font.gilroy_black, FontWeight.Black),
+
+
+    )
