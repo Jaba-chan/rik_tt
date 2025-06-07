3c8ad96bd90d9bfaa89575d26505da67163d7a57
kuzak <kuzakovevgen@gmail.com>
2025-06-07 17:03:31 +0700

    di for new use cases and interactors

--- app/src/main/java/ru/evgenykuzakov/rik_tt/di/domainModule.kt
+++ app/src/main/java/ru/evgenykuzakov/rik_tt/di/domainModule.kt
@@ -1,12 +1,28 @@
 package ru.evgenykuzakov.rik_tt.di
 
 import org.koin.dsl.module
-import ru.evgenykuzakov.domain.interactor.StatisticInteractor
+import ru.evgenykuzakov.domain.interactor.AgeSexStatisticInteractor
+import ru.evgenykuzakov.domain.interactor.DateStatisticsInteractor
+import ru.evgenykuzakov.domain.interactor.MostOftenVisitorsInteractor
+import ru.evgenykuzakov.domain.interactor.VisitorsByTypeInteractor
+import ru.evgenykuzakov.domain.use_case.GetAgeSexStatisticUseCase
+import ru.evgenykuzakov.domain.use_case.GetDateStatisticsUseCase
+import ru.evgenykuzakov.domain.use_case.GetMostOftenVisitorsUseCase
 import ru.evgenykuzakov.domain.use_case.GetStatisticsUseCase
 import ru.evgenykuzakov.domain.use_case.GetUsersUseCase
+import ru.evgenykuzakov.domain.use_case.GetVisitorsByTypeUseCase
 
 val domainModule = module {
     single { GetUsersUseCase(get()) }
     single { GetStatisticsUseCase(get()) }
-    single { StatisticInteractor(get(), get()) }
+
+    single { GetAgeSexStatisticUseCase() }
+    single { GetDateStatisticsUseCase() }
+    single { GetMostOftenVisitorsUseCase() }
+    single { GetVisitorsByTypeUseCase() }
+
+    single { AgeSexStatisticInteractor(get(), get(), get()) }
+    single { DateStatisticsInteractor(get(), get(), get()) }
+    single { MostOftenVisitorsInteractor(get(), get(), get()) }
+    single { VisitorsByTypeInteractor(get(), get(), get()) }
 }
