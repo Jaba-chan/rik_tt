56ad175df55a4ded26643d6188bf2aef6d9267e1
kuzak <kuzakovevgen@gmail.com>
2025-06-07 17:20:27 +0700

    Refactor a StatisticViewModel for new interactors

--- core/domain/src/main/java/ru/evgenykuzakov/domain/model/StatisticUIState.kt
+++ core/domain/src/main/java/ru/evgenykuzakov/domain/model/StatisticUIState.kt
@@ -6,7 +6,7 @@ data class StatisticUIState(
     val visitorsByDate: Resource<List<DateStatistic>> = Resource.Loading(),
     val visitorsByType: Resource<List<VisitorsByType>> = Resource.Loading(),
     val mostOftenVisitors: Resource<List<User>> = Resource.Loading(),
-    val sexAgeStatistic: Resource<List<AgeSexStatisticResult>> = Resource.Loading(),
+    val sexAgeStatistic: Resource<AgeSexStatisticResult> = Resource.Loading(),
 
     val dateFilter: ByDateStatisticFilter = ByDateStatisticFilter.DAY,
     val ageSexFilter: ByAgeSexStatisticFilter = ByAgeSexStatisticFilter.DAY
--- feature/statistic/src/main/java/ru/evgenykuzakov/statistic/StatisticScreenViewModel.kt
+++ feature/statistic/src/main/java/ru/evgenykuzakov/statistic/StatisticScreenViewModel.kt
@@ -2,21 +2,87 @@ package ru.evgenykuzakov.statistic
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
-import ru.evgenykuzakov.common.Resource
-import ru.evgenykuzakov.domain.interactor.StatisticInteractor
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import ru.evgenykuzakov.domain.interactor.AgeSexStatisticInteractor
+import ru.evgenykuzakov.domain.interactor.DateStatisticsInteractor
+import ru.evgenykuzakov.domain.interactor.MostOftenVisitorsInteractor
+import ru.evgenykuzakov.domain.interactor.VisitorsByTypeInteractor
+import ru.evgenykuzakov.domain.model.ByAgeSexStatisticFilter
+import ru.evgenykuzakov.domain.model.ByDateStatisticFilter
 import ru.evgenykuzakov.domain.model.StatisticUIState
+import java.time.LocalDate
 
-class StatisticScreenViewModel(
-    private val statisticInteractor: StatisticInteractor
-): ViewModel() {
-    val uiState: StateFlow<Resource<StatisticUIState>> =
-        statisticInteractor()
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5000),
-                initialValue = Resource.Loading()
-            )
+class class StatisticViewModel(
+    private val dateStatisticsInteractor: DateStatisticsInteractor,
+    private val visitorsByTypeInteractor: VisitorsByTypeInteractor,
+    private val mostOftenVisitorsInteractor: MostOftenVisitorsInteractor,
+    private val ageSexStatisticInteractor: AgeSexStatisticInteractor
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(StatisticUIState())
+    val uiState: StateFlow<StatisticUIState> = _uiState
+
+    init {
+        loadVisitorsByDate()
+        loadVisitorsByType()
+        loadMostOftenVisitors()
+        loadSexAgeStatistic()
+    }
+
+    private fun loadVisitorsByDate(nowDate: LocalDate = LocalDate.now()) {
+        viewModelScope.launch {
+            dateStatisticsInteractor(nowDate, _uiState.value.dateFilter)
+                .collect { result ->
+                    _uiState.update { state ->
+                        state.copy(visitorsByDate = result)
+                    }
+                }
+        }
+    }
+
+    private fun loadVisitorsByType() {
+        viewModelScope.launch {
+            visitorsByTypeInteractor()
+                .collect { result ->
+                    _uiState.update { state ->
+                        state.copy(visitorsByType = result)
+                    }
+                }
+        }
+    }
+
+    private fun loadMostOftenVisitors() {
+        viewModelScope.launch {
+            mostOftenVisitorsInteractor()
+                .collect { result ->
+                    _uiState.update { state ->
+                        state.copy(mostOftenVisitors = result)
+                    }
+                }
+        }
+    }
+
+    private fun loadSexAgeStatistic(nowDate: LocalDate = LocalDate.now()) {
+        viewModelScope.launch {
+            ageSexStatisticInteractor(nowDate, _uiState.value.ageSexFilter)
+                .collect { result ->
+                    _uiState.update { state ->
+                        state.copy(sexAgeStatistic = result)
+                    }
+                }
+        }
+    }
+
+    fun onDateFilterChanged(newFilter: ByDateStatisticFilter) {
+        _uiState.update { it.copy(dateFilter = newFilter) }
+        loadVisitorsByDate()
+    }
+
+    fun onAgeSexFilterChanged(newFilter: ByAgeSexStatisticFilter) {
+        _uiState.update { it.copy(ageSexFilter = newFilter) }
+        loadSexAgeStatistic()
+    }
 }
